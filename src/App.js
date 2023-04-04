e95a8cbb15a7c97093a1150429d91873f43b37b8
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-04-04 16:06:40 +0100

    form created to search keyword

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <div>Hello</div>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center mt-5">
+          Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/sarahnafice/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sarah Nafice
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/SarahShecodes/react-dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
