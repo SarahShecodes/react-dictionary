923c1ab56a024aab7e03d693265b09620c37c179
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-04-06 13:09:33 +0100

    Results component

---
+++ src/Results.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Results(props) {
+  if (props.results) {
+    return <div className="Results">Hello from Results</div>;
+  } else {
+    return null;
+  }
+}
