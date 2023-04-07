5f8b4a936bd0ad1386b92186d995d0d5ec72ea2f
SarahShecodes <123887765+SarahShecodes@users.noreply.github.com>
2023-04-07 13:09:28 +0100

    default keyword to show

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,36 +5,48 @@ import "./Dictionary.css";
 
 //14:19
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("world");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary ">
-      <section>
-        <form className="text-center" onSubmit={search}>
-          <input
-            type="search"
-            placeholder="Type a word here..."
-            onChange={handleKeywordChange}
-          />
-        </form>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary ">
+        <section>
+          <form className="text-center" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Type a word here..."
+              onChange={handleKeywordChange}
+            />
+          </form>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
